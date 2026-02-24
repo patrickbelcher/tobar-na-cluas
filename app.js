e6287f216cd066209cc188c5f28428eabe7339c2
patrickbelcher <166528719+patrickbelcher@users.noreply.github.com>
2026-02-24 18:36:02 +0000

    app.js update

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(expressLayouts);
 app.set("layout", "layout");
+app.disable('x-powered-by');
 
 // Serve static files from "public"
 app.use(express.static(path.join(__dirname, "public")));
